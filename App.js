c1205f10544046d6b627f5a923143ac080614e60
Salam <salam73@gmail.com>
2019-07-15 14:09:50 +0200

    .

--- App.js
+++ App.js
@@ -6,6 +6,11 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
